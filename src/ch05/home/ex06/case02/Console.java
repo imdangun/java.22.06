8ed29eac698bc74dbff1f81eba72627abc53a2c7
601-00 <601-00@DESKTOP-L7MGEPV>
2022-06-24 16:05:10 +0900

    a

--- src/ch05/home/ex06/case02/Console.java
+++ src/ch05/home/ex06/case02/Console.java
@@ -14,14 +14,36 @@ public class Console {
 	}
 	
 	public static String inStr(String msg) {
-		Console.inMsg(msg);
-		return sc.nextLine().trim(); // chainning
+		String val = null;
+		boolean isGood = false;
+		
+		do {
+			Console.inMsg(msg);
+			val = sc.nextLine().trim();
+			if(val.length() > 0) isGood = true;
+			else Console.err("한글자 이상 입력하세요.");
+		} while(!isGood);
+		
+		return val;
 	}
 	
 	public static int inNum(String msg) {
-		Console.inMsg(msg);
-		int num = sc.nextInt(); sc.nextLine();
-		return num;
+		int val = 0;
+		String valStr = null;
+		boolean isGood = false;
+		
+		do {
+			Console.inMsg(msg);
+			valStr = sc.nextLine().trim();
+			
+			if(valStr.length() == 1 &&
+					'1' <= valStr.charAt(0) && valStr.charAt(0) <= '9') {
+				val = Integer.parseInt(valStr);
+				isGood = true;
+			} else Console.err("한자리 자연수를 입력하세요.");
+		} while(!isGood);
+		
+		return val;
 	}
 	
 	public static void info(String msg) {
