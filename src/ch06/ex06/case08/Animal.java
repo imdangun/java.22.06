de062d6361a94fd16e4d8a703287b10292812eb6
601-00 <601-00@DESKTOP-L7MGEPV>
2022-06-28 18:31:39 +0900

    a

---
+++ src/ch06/ex06/case08/Animal.java
@@ -0,0 +1,9 @@
+package ch06.ex06.case08;
+
+public interface Animal {
+	default void eat() {
+		System.out.println("¿‘¿∏∑Œ ∏‘¥Ÿ.");
+	}
+	
+	void move();
+}
---
+++ src/ch06/ex06/case09/Main.java
@@ -0,0 +1,13 @@
+package ch06.ex06.case09;
+
+public class Main {
+	public static void main(String[] args) {
+		Console console = new Console();
+		Browser browser = new Browser();
+		
+		console.out();
+		browser.out();
+		
+		UI.in();
+	}
+}
