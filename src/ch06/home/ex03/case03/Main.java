4c3df59ac301f329818385cfba4ddb19feeaefd2
601-00 <601-00@DESKTOP-L7MGEPV>
2022-06-28 12:50:06 +0900

    ㅁ

--- src/ch06/home/ex03/case03/Main.java
+++ src/ch06/home/ex03/case03/Main.java
@@ -2,8 +2,8 @@ package ch06.home.ex03.case03;
 
 public class Main {
 	public static void main(String[] args) {
-		User user1 = new User();
-		User user2 = new User();
+		User user1 = new User("최한석", 25);
+		User user2 = new User("한아름", 45);
 		
 		System.out.println(user1);
 		System.out.println(user2);
