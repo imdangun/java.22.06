809aa4d69ea07a968693f450345cce4a6f10f12a
601-00 <601-00@DESKTOP-L7MGEPV>
2022-06-30 18:19:52 +0900

    a

---
+++ src/ch07/ex04/case05/presentation/ScoreIo.java
@@ -0,0 +1,31 @@
+package ch07.ex04.case05.presentation;
+
+import ch07.ex04.case05.domain.Score;
+import ch07.ex04.case05.service.ScoreService;
+
+public class ScoreIo {
+	private ScoreService scoreService;
+	
+	public ScoreIo(ScoreService scoreService) {
+		this.scoreService = scoreService;
+	}
+	
+	public void play() {
+		int korScore = 0;
+		int engScore = 0;
+		int mathScore = 0;		
+		
+		for(int i = 0; i < 5; i++) {
+			korScore = Console.inNum("국어 점수를 입력하세요.");
+			engScore = Console.inNum("영어 점수를 입력하세요.");
+			mathScore = Console.inNum("수학 점수를 입력하세요.");
+			
+			scoreService.addScore(new Score(korScore, engScore, mathScore));
+		}		
+		
+		Score[] scores = scoreService.getScores();
+		System.out.println();
+		for(Score score: scores)
+			System.out.println(score);
+	}
+}
--- src/ch07/home/ex04/case01/MaxMin.java
+++ src/ch07/home/ex04/case01/MaxMin.java
@@ -4,6 +4,13 @@ public class MaxMin {
 	public static void main(String[] args) {
 		int[] scores = {77, 88, 91, 33, 100, 55, 95};
 		
+		int max = scores[0];
+		int min = scores[0];
+		
+		for(int score: scores) {
+			if(score > max) max = score;
+			if(score < min) min = score;
+		}
 		
 		System.out.printf("%d %d", max, min);
 	}
